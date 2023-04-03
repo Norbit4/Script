1d897c54fdb126d11f0988e0298c7964a58dd2d3
Norbert Śleziak <norr.bitt@gmail.com>
2023-04-03 13:51:18 +0200

    add log

--- script.sh
+++ script.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+script_name=$(basename "$0")
+
+if [ "$1" == "--date" ]; then
+    date
+elif [ "$1" == "--logs" ]; then
+    if [ -z "$2" ]; then
+        for i in {1..100}
+        do
+            echo "Nazwa pliku: log$i.txt" > log$i.txt
+            echo "Nazwa skryptu: $script_name" >> log$i.txt
+            echo "Data utworzenia: $(date)" >> log$i.txt
+        done
+        echo "Utworzono 100 plików log"
+    else
+        for i in $(seq 1 $2)
+        do
+            echo "Nazwa pliku: log$i.txt" > log$i.txt
+            echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
+            echo "Data utworzenia: $(date)" >> log$i.txt
+        done
+        echo "Utworzono $2 plików log"   
+    fi
+fi
